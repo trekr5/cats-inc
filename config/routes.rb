ff47747a7f8156cb397bf85daffad30f046b4181
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-04-07 18:28:25 +0100

    routes exercise

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,21 @@ CatsInc::Application.routes.draw do
   devise_for :users
 
   # Add the nessessary routes here.
+  
+  # Map /items to the path controller items and action/view index
+  get "/items", to: "items#index"
 
+  get "/items/:id", to: "items#show"
+
+# Named routes make it easier for you to see
+  get "/homepage/", to: "homepage#index"
+
+  # The DELETE verb is used when deleting from a page
+  delete "/stockalerts/:id", to: "stock_alerts#destroy"
+ 
+ # Named route 
+  get "/stockalerts/:id", to: "stock_alerts#show", as: :stock_alert 
+  post "/stockalerts/", to: "stock_alerts#create"
+
+  get "/admin/dashboard", to: "admin/dashboard#index" 
 end
